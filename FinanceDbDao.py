cf2cb461efd084db8a2947ebbd12c97f93c37349
Rhea_Dmello <rheadmello2004@gmail.com>
2025-07-31 18:27:21 +0530

    added

--- FinanceDbDao.py
+++ FinanceDbDao.py
@@ -4,7 +4,6 @@ import pymysql
 from pymysql.cursors import DictCursor
 from model import Transaction
 
-# Load environment variables
 load_dotenv()
 
 class FinanceMySQLService:
@@ -72,7 +71,6 @@ class FinanceMySQLService:
             print(self.getMessage("MSG_ADD_TRANSACTION_FAIL"))
 
     def viewTransactions(self):
-        """Fetch and display all transactions without filters"""
         qry = f"SELECT * FROM {self.table} ORDER BY transaction_id"
 
         try:
@@ -95,16 +93,51 @@ class FinanceMySQLService:
                     date=trans_date,
                     type=transaction["type"]
                 ))
-            # print(f"{'-'*50}\n")
 
         except Exception as e:
             print(f"Error fetching all transactions: {e}")
 
     def filterByCategory(self, category):
-        self.viewTransactions(category=category)
+        qry=f"SELECT * FROM {self.table} WHERE category=%s ORDER BY date"
+        try:
+            self.cursor.execute(qry,(category,))
+            transactions=self.cursor.fetchall()
+            if not transactions:
+                print(f"\nNo transactions found for category:{category}")
+                return
+            print(f"\n{'-'*50}")
+            print(f"Transactions in Category:{category}")
+            print(f"\n{'-'*50}")
+            for transaction in transactions:
+                print(Transaction(
+                    transaction_id=transaction["transaction_id"],
+                    amount=transaction["amount"],
+                    category=transaction["category"],
+                    date=transaction["date"],
+                    type=transaction["type"]))
+        except Exception as e:
+            print(f"Error:{e}")
 
     def filterByDate(self, date):
-        self.viewTransactions(date=date)
+        qry=f"SELECT * FROM {self.table} WHERE date=%s ORDER BY transaction_id"
+        try:
+            self.cursor.execute(qry,(date,))
+            transactions=self.cursor.fetchall()
+            if not transactions:
+                print(f"\nNo transactions found on date:{date}")
+                return
+            print(f"\n{'-'*50}")
+            print(f"Transactions on date:{date}")
+            print(f"\n{'-'*50}")
+            for transaction in transactions:
+                print(Transaction(
+                    transaction_id=transaction["transaction_id"],
+                    amount=transaction["amount"],
+                    category=transaction["category"],
+                    date=transaction["date"],
+                    type=transaction["type"]))
+        except Exception as e:
+            print(f"Error:{e}")
 
     def calculateMonthlyBalance(self, month, year):
         qry = f"""
@@ -127,9 +160,9 @@ class FinanceMySQLService:
 
             print(f"\nMonthly Summary: {month_name} {year}")
             print(f" {'='*40}")
-            print(f" Income  : ₹{total_income:,.2f}")
-            print(f" Expense : ₹{total_expense:,.2f}")
-            print(f" Balance : ₹{balance:,.2f}")
+            print(f" Income  : {total_income:,.2f}")
+            print(f" Expense : {total_expense:,.2f}")
+            print(f" Balance : {balance:,.2f}")
             print(f" {'='*40}\n")
 
         except Exception as e:
